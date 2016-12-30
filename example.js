fdc7126a705752f9c2aa4f9bfe12581c4b4ecdd6
Michael Dawson <mdawson@devrus.com>
2016-12-30 12:01:15 -0500

    fix formatting of example

--- example.js
+++ example.js
@@ -5,33 +5,33 @@ const browseServer = require('./lib/index.js');
 var listContent = function(queue, url, options, err) {
   var root = queue.shift();
   browseServer(
-    root, 
+    root,
     url,
     {},
     function(err, result) {
-    if (result.container) {
-      for (let i = 0; i < result.container.length; i++) {
-        if (result.container[i].id != 'Video/temp') {
-          queue.push(result.container[i].id);
-          console.log('Container:' + result.container[i].id);
+      if (result.container) {
+        for (let i = 0; i < result.container.length; i++) {
+          if (result.container[i].id != 'Video/temp') {
+            queue.push(result.container[i].id);
+            console.log('Container:' + result.container[i].id);
+          }
         }
       }
-    }
 
-    if (result.item) {
-      for (let i = 0; i < result.item.length; i++) {
- //       if (result.item[i].title.indexOf('Big_Bang_Theory') != -1) {
+      if (result.item) {
+        for (let i = 0; i < result.item.length; i++) {
           console.log('Item:' + result.item[i].title);
- //       }
+        }
       }
-    }
-    if (queue.length >0) {
-      listContent(queue, url, options, err);
-    }
-  });
-}
+      if (queue.length >0) {
+        listContent(queue, url, options, err);
+      }
+    });
+  }
 
-var queue = new Array();
-queue.push('New(auto)');
-listContent(queue,'http://10.1.1.176:49081/dev/b9a87696-f016-4a54-81b3-75f57185a385/svc/upnp-org/ContentDirectory/action',
-                  {}, function(){});
+  var queue = new Array();
+  queue.push('New(auto)');
+  listContent(queue,
+              'http://10.1.1.176:49081/dev/b9a87696-f016-4a54-81b3-75f57185a385/svc/upnp-org/ContentDirectory/action',
+              {},
+              function(){});
